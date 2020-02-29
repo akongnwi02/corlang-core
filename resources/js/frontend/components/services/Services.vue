6964dfa1c222d9c4d4e79ced5e77d9d99c136349
devert <akongnwi02@gmail.com>
2020-03-01 00:37:30 +0100

    - Updated the layout for the services

---
+++ resources/js/frontend/components/services/Services.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="row">
+        <div v-for="service in services" class="col-md-4">
+            <div class="thumbnail">
+                <img :src="service.logo_url" :alt="service.name" style="width:50%">
+                <div class="caption">
+                    <p>{{service.name}}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Services",
+        props: ['services']
+    }
+</script>
+
+<style scoped>
+
+</style>
