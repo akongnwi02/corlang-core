e9d615d661e24a223b055749e822d592eb085e43
devert <akongnwi02@gmail.com>
2020-05-17 10:42:48 +0100

    Little cleanup

--- resources/views/backend/includes/header.blade.php
+++ resources/views/backend/includes/header.blade.php
@@ -33,22 +33,22 @@
                 @include('includes.partials.lang')
             </li>
         @endif
-        <li class="nav-item d-md-down-none">
-            <a class="nav-link" href="#">
-                <i class="icon-bell"></i>
-                <span class="badge badge-pill badge-danger">5</span>
-            </a>
-        </li>
-        <li class="nav-item d-md-down-none">
-            <a class="nav-link" href="#">
-                <i class="icon-list"></i>
-            </a>
-        </li>
-        <li class="nav-item d-md-down-none">
-            <a class="nav-link" href="#">
-                <i class="icon-location-pin"></i>
-            </a>
-        </li>
+        {{--<li class="nav-item d-md-down-none">--}}
+            {{--<a class="nav-link" href="#">--}}
+                {{--<i class="icon-bell"></i>--}}
+                {{--<span class="badge badge-pill badge-danger">5</span>--}}
+            {{--</a>--}}
+        {{--</li>--}}
+        {{--<li class="nav-item d-md-down-none">--}}
+            {{--<a class="nav-link" href="#">--}}
+                {{--<i class="icon-list"></i>--}}
+            {{--</a>--}}
+        {{--</li>--}}
+        {{--<li class="nav-item d-md-down-none">--}}
+            {{--<a class="nav-link" href="#">--}}
+                {{--<i class="icon-location-pin"></i>--}}
+            {{--</a>--}}
+        {{--</li>--}}
         <li class="nav-item dropdown">
           <a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
             <img src="{{ $logged_in_user->picture }}" class="img-avatar" alt="{{ $logged_in_user->email }}" width="30" height="30">
@@ -58,35 +58,35 @@
             <div class="dropdown-header text-center">
               <strong>Account</strong>
             </div>
-            <a class="dropdown-item" href="#">
-              <i class="fa fa-bell"></i> Updates
-              <span class="badge badge-info">42</span>
-            </a>
-            <a class="dropdown-item" href="#">
-              <i class="fa fa-envelope"></i> Messages
-              <span class="badge badge-success">42</span>
-            </a>
-            <a class="dropdown-item" href="#">
-              <i class="fa fa-tasks"></i> Tasks
-              <span class="badge badge-danger">42</span>
-            </a>
-            <a class="dropdown-item" href="#">
-              <i class="fa fa-comments"></i> Comments
-              <span class="badge badge-warning">42</span>
-            </a>
-            <div class="dropdown-header text-center">
-              <strong>Settings</strong>
-            </div>
-            <a class="dropdown-item" href="#">
-              <i class="fa fa-user"></i> Profile
-            </a>
-            <a class="dropdown-item" href="#">
-              <i class="fa fa-wrench"></i> Settings
-            </a>
-            <a class="dropdown-item" href="#">
-              <i class="fa fa-file"></i> Projects
-              <span class="badge badge-primary">42</span>
-            </a>
+            {{--<a class="dropdown-item" href="#">--}}
+              {{--<i class="fa fa-bell"></i> Updates--}}
+              {{--<span class="badge badge-info">42</span>--}}
+            {{--</a>--}}
+            {{--<a class="dropdown-item" href="#">--}}
+              {{--<i class="fa fa-envelope"></i> Messages--}}
+              {{--<span class="badge badge-success">42</span>--}}
+            {{--</a>--}}
+            {{--<a class="dropdown-item" href="#">--}}
+              {{--<i class="fa fa-tasks"></i> Tasks--}}
+              {{--<span class="badge badge-danger">42</span>--}}
+            {{--</a>--}}
+            {{--<a class="dropdown-item" href="#">--}}
+              {{--<i class="fa fa-comments"></i> Comments--}}
+              {{--<span class="badge badge-warning">42</span>--}}
+            {{--</a>--}}
+            {{--<div class="dropdown-header text-center">--}}
+              {{--<strong>Settings</strong>--}}
+            {{--</div>--}}
+              <a class="dropdown-item" href="{{ route('frontend.user.account') }}">
+                  <i class="fa fa-user"></i> @lang('navs.frontend.user.profile')
+              </a>
+            {{--<a class="dropdown-item" href="#">--}}
+              {{--<i class="fa fa-wrench"></i> Settings--}}
+            {{--</a>--}}
+            {{--<a class="dropdown-item" href="#">--}}
+              {{--<i class="fa fa-file"></i> Projects--}}
+              {{--<span class="badge badge-primary">42</span>--}}
+            {{--</a>--}}
             <div class="divider"></div>
             <a class="dropdown-item" href="{{ route('frontend.auth.logout') }}">
                 <i class="fas fa-lock"></i> @lang('navs.general.logout')
@@ -95,10 +95,10 @@
         </li>
     </ul>
 
-    <button class="navbar-toggler aside-menu-toggler d-md-down-none" type="button" data-toggle="aside-menu-lg-show">
-        <span class="navbar-toggler-icon"></span>
+    <button disabled class="navbar-toggler aside-menu-toggler d-md-down-none" type="button" data-toggle="aside-menu-lg-show">
+        {{--<span class="navbar-toggler-icon"></span>--}}
     </button>
-    <button class="navbar-toggler aside-menu-toggler d-lg-none" type="button" data-toggle="aside-menu-show">
-        <span class="navbar-toggler-icon"></span>
+    <button disabled class="navbar-toggler aside-menu-toggler d-lg-none" type="button" data-toggle="aside-menu-show">
+        {{--<span class="navbar-toggler-icon"></span>--}}
     </button>
 </header>
