b6c71764e5fab3f7963e370b27decbbb628bbacd
devert <akongnwi02@gmail.com>
2020-06-18 19:32:39 +0100

    Little clean up

--- app/Models/Traits/Attributes/PaymentMethodAttribute.php
+++ app/Models/Traits/Attributes/PaymentMethodAttribute.php
@@ -17,7 +17,7 @@ trait PaymentMethodAttribute
     public function getLogoLabelAttribute()
     {
         if ($this->is_default) {
-            return "<img class='navbar-brand-full img-fluid' src='/img/backend/brand/logo/logo-company-profile.png' width='30' height='30' style='border-radius: 50%' alt='$this->name'>";
+            return "<img class='navbar-brand-full img-fluid' src='/img/backend/brand/logo/logo-company-profile.png' width='50' height='50' style='border-radius: 50%' alt='$this->name'>";
         } else {
             return $this->service->logo_label;
         }
--- resources/views/frontend/user/account/tabs/topup.blade.php
+++ resources/views/frontend/user/account/tabs/topup.blade.php
@@ -17,14 +17,14 @@
         <tbody>
             @foreach($topup_methods as $key => $method)
                 <tr>
-                    <td>{!! @$method->logo_label !!}</td>
-                    <td style="min-width: 100px; text-align: center" class="alert alert-warning">{{$method->name}}</td>
-                    <td><input placeholder="{{$method->placeholder_text}}" id="account-{{$method->uuid}}" style="min-width:100px" type="text" name="topup_config[{{ $key }}][account]" value="{{@$logged_in_user->getTopupAccount($method)->account}}" class="form-control" {{@$logged_in_user->getTopupAccount($method)->is_confirmed? 'disabled': ''}}/></td>
+                    <td style="height: 50px; width: 50px;">{!! @$method->logo_label !!}</td>
+                    <td>{{$method->name}}</td>
+                    <td><input placeholder="{{$method->placeholder_text}}" id="account-{{$method->uuid}}" type="text" name="topup_config[{{ $key }}][account]" value="{{@$logged_in_user->getTopupAccount($method)->account}}" class="form-control" {{@$logged_in_user->getTopupAccount($method)->is_confirmed? 'disabled': ''}}/></td>
                     <td><input type="hidden" id="uuid" name="topup_config[{{$key}}][method_id]" value="{{$method->uuid}}"/></td>
                     @if(@$logged_in_user->getTopupAccount($method)->is_confirmed)
-                        <td><span class='badge badge-success'>@lang('labels.general.yes')</span></td>
+                        <td><i class='badge badge-success'>@lang('labels.general.yes')</i></td>
                     @else
-                        <td><span class='badge badge-danger'>@lang('labels.general.no')</span></td>
+                        <td><i class='badge badge-danger'>@lang('labels.general.no')</i></td>
                     @endif
                 </tr>
             @endforeach
@@ -36,7 +36,7 @@
 </div>
 <div class="row">
     <div class="col text-right">
-        {{ form_submit(__('buttons.general.continue')) }}
+        {{ form_submit(__('buttons.general.submit')) }}
     </div><!--row-->
 </div><!--row-->
 {{ html()->form()->close() }}
